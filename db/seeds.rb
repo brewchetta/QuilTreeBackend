9b38e101f5a748665cd161e3a019a3eff71b6bdd
brewchetta <chett.tiller@gmail.com>
2018-12-31 11:39:40 -0500

    Added more variety to seed data including more images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
 names = ['Chett', 'Tiller', 'Jesse', 'Rice-Evans', 'Mesha', 'Maren', 'Zephyr', 'Matt', 'Owens', 'Zefyr', 'Octavia', 'Butler', 'Ursula', 'Le Guin', 'Colson', 'Whitehead', 'Brian', 'K. Vaughn', 'Noelle', 'Noelle', 'Stevenson', 'Chimamanda', 'Adiche', 'Virginia', 'Woolf', 'Jean Luc', 'Picard', 'Geordi', 'Beverly', 'Crusher', 'Wesley', 'Seven of Nine', 'Janeway', 'Katherine', 'Data', 'Sisko', 'Benjamin', 'Kira', 'Nerise', 'Misty', 'Brock', 'Ash', 'Ketchum', 'Gary', 'Surge', 'Leonard', 'Nimoy', 'Bilbo', 'Baggins', 'Frodo', 'Samwise', 'Gandalf', 'Zeus', 'Aphrodite', 'Thor', 'Odin', 'Nidhogg', 'Surtur', 'Lady Gaga', 'Lorde']
 
-titles = ['LumberJanes', 'Ms Marvel', 'Captain Marvel', 'Black Panther', 'Thor', 'Paper Girls', 'Earthsea', 'Monstress', 'The Golden Compass', 'Purple Hibiscus', 'Sugar Run', 'Navigator', 'Soft Switch', 'Lilliths Brood', 'Half of a Yellow Sun', 'Moby Dick', 'Orlando', 'A Christmas Carol', 'For Whom the Bell Tolls', 'Carebears', 'She-Ra', 'Ninja Turtles', 'Necronomicon', 'Death of a Salesman', 'To Kill A Mockingbird', 'The Crucible', 'Saga', 'Nimona', 'A Wrinkle in Time', 'Mars Attacks', 'The Abyss', 'The Hobbit', 'The Two Towers', 'Return of the King', 'The Fellowship of the Ring', 'Trainers Guide to Pokemon', 'Dragonball Z', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Adama', 'Skywalker', 'Luke', 'Anakin', 'Obi Wan', 'Leia']
+titles = ['LumberJanes', 'Ms Marvel', 'Captain Marvel', 'Black Panther', 'Thor', 'Paper Girls', 'Earthsea', 'Monstress', 'The Golden Compass', 'Purple Hibiscus', 'Sugar Run', 'Navigator', 'Soft Switch', 'Lilliths Brood', 'Half of a Yellow Sun', 'Moby Dick', 'Orlando', 'A Christmas Carol', 'For Whom the Bell Tolls', 'Carebears', 'She-Ra', 'Ninja Turtles', 'Necronomicon', 'Death of a Salesman', 'To Kill A Mockingbird', 'The Crucible', 'Saga', 'Nimona', 'A Wrinkle in Time', 'Mars Attacks', 'The Abyss', 'The Hobbit', 'The Two Towers', 'Return of the King', 'The Fellowship of the Ring', 'Trainers Guide to Pokemon', 'Dragonball Z', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Adama', 'Skywalker', 'Luke', 'Anakin', 'Obi Wan', 'Leia', 'Southern Cross', 'Y the Last Man']
+
+images = [
+  'https://images.unsplash.com/photo-1496062851822-495efe173ecd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  'https://pmcvariety.files.wordpress.com/2018/09/care-bears.jpg?w=1000&h=563&crop=1',
+  'https://images.unsplash.com/photo-1518713337836-f828c91becc0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80',
+  'https://images.unsplash.com/photo-1459609336665-6ef3866c8930?ixlib=rb-1.2.1&auto=format&fit=crop&w=1506&q=80',
+  'https://images.unsplash.com/photo-1522682078546-47888fe04e81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  'https://images.unsplash.com/photo-1467767079704-1e5389c42c9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1510&q=80'
+]
+
+# Image credits in order
+# Fancy Crave | @fancycrave
+# Carebears or whatever
+# Rich Dahlgren | @clamstorm
+# Hannes Wolf | @hannes_wolf
+# Hayes Potter | @hayespotter
+# Ashley Knedler | @ashkned
 
 puts 'Burning all the books to start over'
 User.all.each { |user| user.delete}
@@ -18,6 +35,6 @@ puts 'Writers chained to their desks'
 puts 'Thinking of titles'
 puts '...'
 1000.times do
-  Story.create(title: titles[rand(titles.length - 1)] + ' vol' + rand(100).to_s, description: "A retelling of #{names[rand(names.length - 1)]}'s trials and tribulations.", image: 'https://pmcvariety.files.wordpress.com/2018/09/care-bears.jpg?w=1000&h=563&crop=1', user_id: User.all[rand(User.all.length - 1)].id)
+  Story.create(title: titles[rand(titles.length - 1)] + ' vol' + rand(100).to_s, description: "A retelling of #{names[rand(names.length - 1)]}'s trials and tribulations.", image: images[rand(images.length - 1)], user_id: User.all[rand(User.all.length - 1)].id)
 end
 puts 'Thought of some titles'
